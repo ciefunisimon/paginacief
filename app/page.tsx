ef1fdc3028c23cc8f82b98bb044dacab5af9777f
ciefunisimon <ciefunisimon@gmail.com>
2026-03-10 15:56:37 -0500

    update page
    update page

--- app/page.tsx
+++ app/page.tsx
@@ -167,6 +167,18 @@ export default function HomePage() {
                   información sobre dinámicas fronterizas.
                 </p>
               </Link>
+
+              <Link
+                href="/redes_colaborativas"
+                className="block p-6 sm:p-8 rounded-2xl shadow-md border border-[#68AB6A]/30 hover:shadow-lg transition text-center bg-gradient-to-b from-white to-gray-50"
+              >
+                <h3 className="text-2xl font-semibold text-[#68AB6A] mb-4">
+                  Redes de Colaboración Científica
+                </h3>
+                <p className="text-gray-700">
+                 Nodos de articulación para el intercambio de conocimientos, fortalecimiento de capacidades investigativas y desarrollo de proyectos de impacto.
+                </p>
+              </Link>
             </div>
           </div>
         </section>
@@ -341,7 +353,7 @@ export default function HomePage() {
     <div className="grid grid-cols-1 sm:grid-cols-2 gap-6 sm:gap-8 text-left">
       <div className="p-6 sm:p-8 border border-white/20 rounded-2xl shadow-lg bg-white/10 backdrop-blur-sm">
         <h3 className="text-xl font-bold mb-2 text-[white]">
-          Últimas Publicaciones
+          Ultimas Publicaciones
         </h3>
 
         <hr className="my-3 border-white/30" />
@@ -515,4 +527,3 @@ export default function HomePage() {
     </>
   );
 }
-
